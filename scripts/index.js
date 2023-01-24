34439aa629eb710e0c7e54ee90d20cbb3a93f615
yambikov <yambikov@yandex.ru>
2023-01-25 00:04:42 +0300

    html css js are ready

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,28 @@
 
 /* Пишем правило для открытия попапа */
+let popup = document.querySelector('.popup'); // находим popup
+let editButton = document.querySelector('.profile__edit-button'); // находим иконку открытия попапа 
+let closeButton = document.querySelector('.popup__close-icon'); // находим иконку закрытия попапа
+let newName = document.querySelector('.profile__title');  //находим текст "Имя"
+let newJob = document.querySelector('.profile__subtitle'); //находим текст "Роль"
+let formElement = document.querySelector('.popup__content'); // находим форму с инпутами
+let nameInput = document.querySelector('[name="profile-edit-name"]'); //находим значение инпута "Имя"
+let jobInput = document.querySelector('[name="profile-edit-role"]'); //находим значение инпута "Роль"
 
-// находим popup
-let popup = document.querySelector('.popup');
-// находим кнопку  
-let editButton = document.querySelector('.profile__edit-button'); 
-// создаем функцию для клика по кнопке
-function openPopup() {                           
+
+
+/* Пишем функцию для открытия попапа */
+function openPopup() {
   popup.classList.add('popup_opened'); // функция добавляет класс popup_opened к попапу
-  
+  nameInput.value = newName.textContent; // присваиваем значению инпута "Имя" значение текста "Имя"
+  jobInput.value = newJob.textContent; // аналогично
 }
-// вешаем слушателя на кнопку и прописываем событие (клик) и сценарий (запустить функцию)
-editButton.addEventListener('click', openPopup);    
+
+editButton.addEventListener('click', openPopup); // вешаем слушателя на кнопку открытия попапа и прописываем событие (клик) и сценарий (запустить функцию открытия попапа)   
 
 
 
-/* Пишем правило для открытия попапа */
-
-let closeButton = document.querySelector('.popup__close-icon');
-
+/* Пишем функцию для закрытия попапа через иконку закрытия попапа */
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
@@ -28,27 +32,12 @@ closeButton.addEventListener('click', closePopup);
 
 /* Пишем правио для сохранения содержимого */
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__content');
-// Находим поля формы в DOM
-let nameInput = document.querySelector('[name="profile-edit-name"]'); //[name="your-selector-name-here"]
-let jobInput = document.querySelector('[name="profile-edit-role"]');
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. Так мы можем определить свою логику отправки. О том, как это делать, расскажем позже.
- 
-  // Получите значение полей jobInput и nameInput из свойства value
-  console.log(nameInput.value);
-  console.log(jobInput.value);
-  // Выберите элементы, куда должны быть вставлены значения полей
-  let newName = document.querySelector('.profile__title');
-  let newJob = document.querySelector('.profile__subtitle');
-  // Вставьте новые значения с помощью textContent
   newName.textContent = nameInput.value;
   newJob.textContent = jobInput.value;
   closePopup()
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
+formElement.addEventListener('submit', handleFormSubmit); // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
