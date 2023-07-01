f03bc79dd02022a3f94878044a199257d62eb00f
yambikov <yambikov@yandex.ru>
2023-07-01 14:57:43 +0300

    #67 save

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,57 @@
-const path = require('path');
+const path = require('path'); // плагин чтобы не указывать относительный путь к path - директории
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // плагин подключающий скрипты в html автоматически
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // плагин, который будет каждый раз при сборке проекта удалять содержимое папки dist
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');  // берёт много css-файлов и объединяет их в один
+
 
 module.exports = {
   entry: { main: './src/scripts/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '.main.js',
+    filename: 'main.js',
     publicPath: '',
   },
   mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    compress: true, // это ускорит загрузку в режиме разработки
+    port: 8088, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+
+    open: true // сайт будет открываться сам при запуске npm run dev
+  },
+  module: {
+    rules: [ // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        use: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
+      },
+      {
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      {
+        // применять это правило только к CSS-файлам
+        test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader'
+        }]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html' // путь к файлу index.html
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ]
 }
